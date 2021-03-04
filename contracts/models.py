8f387a229046e4e5bd282ac028bc97daf8eb5f7b
Borui Tao <borui.tao@mail.mcgill.ca>
2021-03-04 13:49:25 +0800

    create model

--- contracts/models.py
+++ contracts/models.py
@@ -1,11 +1,33 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 import datetime
-# Create your models here.
+# Create your models here
 class Contract(models.model):
-    start_date = models.DateField('start date')
-    end_date = models.DateField('end date')
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    start_date = models.DateField('start date') # 开始日期
+    end_date = models.DateField('end date') # 截止日期
+    sign_date = models.DateField('sign date') # 签订日期
     floor_num = models.IntegerField('floor number') # 楼层
     store_loc_code = models.CharField('store location code')# 铺位号
-    unit_price = models.DecimalField('unit price') # 单价
-    
+    region = models.CharField('region') # 铺位所在区
+    unit_price = models.DecimalField('monthly unit price') # 单价
+    area = models.DecimalField('area') # 面积
+    monthly_price = models.DecimalField('monthly price') # 月总价
+    yearly_price = models.DecimalField('yearly price') # 全年总价
+    payment_cycle = models.IntegerField('payment cycle') # 收款周期按月计
+    business = models.CharField('business') # 经营项目
+    category = models.ChoiceField('business category') # 业态
+    promotion_price = models.DecimalField('promotion price') # 推广费
+    deposit = models.DecimalField('deposit') # 铺位总定金
+    perf_bond = models.DecimalField('total performance bond') # 履约保证金总价
+    water_bill = models.DecimalField('monthly water unit price') # 每月单价水费
+    elect_bill = models.DecimalField('monthly electricity unit price') # 每月单价电费
 
+
+class Company(models.model):
+    owner_name = models.CharField('owner name') # 法人姓名
+    owner_phone_num = PhoneNumberField(null=False, blank=False, unique=True) # 电话
+    owner_address = models.CharField('owner address') # 单位或家庭地址
+    owner_id = modles.CharField('owner id') # 身份证号
+    company_name = models.CharField('company name') # 公司名
+    core_business = models.CharField('core business') # 公司业务
--- huahai/wsgi.py
+++ huahai/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for huahai_erp project.
+WSGI config for huahai project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'huahai_erp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'huahai.settings')
 
 application = get_wsgi_application()
